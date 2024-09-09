fce21d6d8fb68ff7d9d99227e2469187b75221f4
Andrei <andreykapas@gmail.com>
2024-09-09 12:50:13 +0300

    Divide Stats to 4 blocks

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,7 +1,19 @@
 const Stats = () => {
   return (
     <section className="stats">
-      Stats
+      <Stat description={'Words'} value={0} />
+      <Stat description={'Characters'} value={0} />
+      <Stat description={'Instagram'} value={280} />
+      <Stat description={'Facebook'} value={2200} />
+    </section>
+  );
+};
+
+const Stat = ({ description, value }) => {
+  return (
+    <section className="stat">
+      <span className="stat__number">{value}</span>
+      <h2 className="second-heading">{description}</h2>
     </section>
   );
 };
